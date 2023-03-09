c5e80e7d6337b5a37dc728524e3a94bf3ce46b88
vosrhd9 <vosrhd9@gmail.com>
2023-03-10 02:27:31 +0300

    Upt langue

--- Overlay/main.cpp
+++ Overlay/main.cpp
@@ -1,6 +1,6 @@
 #include <Windows.h>
 #include <chrono>
-#include <Dwmapi.h> 
+#include <Dwmapi.h>
 #pragma comment(lib, "dwmapi.lib")
 
 const MARGINS margins = { -1 ,-1, -1, -1 };
@@ -12,14 +12,14 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     switch (msg)
     {
     case WM_HOTKEY:
-        if (wParam == 1) // При нажатии на горячую клавишу ALT + Z
+        if (wParam == 1) // When the hotkey ALT + Z is pressed
         {
-            isVisible = !isVisible; // Инвертируем значение переменной isVisible
-            ShowWindow(hwnd, isVisible ? SW_SHOW : SW_HIDE); // Показываем/скрываем окно в зависимости от значения isVisible
+            isVisible = !isVisible; // Invert the value of the isVisible variable
+            ShowWindow(hwnd, isVisible ? SW_SHOW : SW_HIDE); // Show/hide the window based on the value of isVisible
         }
-        else if (wParam == 2) // При нажатии на горячую клавишу ALT + X
+        else if (wParam == 2) // When the hotkey ALT + X is pressed
         {
-            // Закрытие окна
+            // Close the window
             DestroyWindow(hwnd);
         }
         break;
@@ -50,10 +50,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     HWND hwnd;
     MSG Msg;
 
-    // Регистрация класса окна
+    // Register window class
     wc.cbSize = sizeof(WNDCLASSEX);
     wc.style = CS_HREDRAW | CS_VREDRAW;
-    wc.lpfnWndProc = WndProc; // Указываем обработчик сообщений
+    wc.lpfnWndProc = WndProc; // Set message handler
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hInstance = hInstance;
@@ -66,9 +66,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     RegisterClassEx(&wc);
 
-    // Создание окна
+    // Create window
     hwnd = CreateWindowEx(
-        WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW | WS_EX_TOPMOST, // <--- добавляем флаг WS_EX_TOOLWINDOW
+        WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW | WS_EX_TOPMOST, // <--- Add the WS_EX_TOOLWINDOW flag
         g_szClassName,
         g_szClassName,
         WS_POPUP | WS_VISIBLE,
@@ -78,18 +78,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     SetLayeredWindowAttributes(hwnd, RGB(0, 0, 0), 255, LWA_ALPHA);
     DwmExtendFrameIntoClientArea(hwnd, &margins);
 
-    // Регистрация горячей клавиши
-    RegisterHotKey(hwnd, 1, MOD_ALT, 0x5A); // Здесь 0x5A соответствует клавише Z
-    RegisterHotKey(hwnd, 2, MOD_ALT, 0x58); // Здесь 0x58 соответствует клавише X
+    // Register hotkeys
+    RegisterHotKey(hwnd, 1, MOD_ALT, 0x5A); // Here 0x5A corresponds to the Z key
+    RegisterHotKey(hwnd, 2, MOD_ALT, 0x58); // Here 0x58 corresponds to the X key
 
-    // Цикл обработки сообщений
+    // Message loop
     while (GetMessage(&Msg, NULL, 0, 0) > 0)
     {
         TranslateMessage(&Msg);
         DispatchMessage(&Msg);
     }
 
-    // Удаление горячей клавиши
+    // Unregister hotkeys
     UnregisterHotKey(hwnd, 1);
     UnregisterHotKey(hwnd, 2);
 
